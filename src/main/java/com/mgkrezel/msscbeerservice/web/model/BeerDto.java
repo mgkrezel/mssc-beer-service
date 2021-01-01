b81c063cb79f9108765a9c43edc9a73430f159fc
mgkrezel <40792249+mgkrezel@users.noreply.github.com>
2021-01-01 17:55:48 +0100

    rest doc config

--- src/main/java/com/mgkrezel/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/mgkrezel/msscbeerservice/web/model/BeerDto.java
@@ -1,14 +1,8 @@
 package com.mgkrezel.msscbeerservice.web.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -32,6 +26,7 @@ public class BeerDto {
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String beerName;
 
     @NotNull
@@ -45,5 +40,6 @@ public class BeerDto {
     @Positive
     private BigDecimal price;
 
+    @Positive
     private Integer quantityOnHand;
 }
--- src/test/java/com/mgkrezel/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/mgkrezel/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,22 +1,33 @@
 package com.mgkrezel.msscbeerservice.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mgkrezel.msscbeerservice.web.model.BeerDto;
 import com.mgkrezel.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(uriScheme = "https")
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
@@ -28,18 +39,53 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(
+                        document("v1/beer-get",
+                                pathParameters(
+                                        parameterWithName("beerId").description("UUID of desired beer to get")
+                                ),
+                                responseFields(
+                                        fieldWithPath("id").description("Id of Beer"),
+                                        fieldWithPath("version").description("Version number"),
+                                        fieldWithPath("createdDate").description("Date created"),
+                                        fieldWithPath("lastModifiedDate").description("Date updated"),
+                                        fieldWithPath("beerName").description("Beer name"),
+                                        fieldWithPath("beerStyle").description("Beer style"),
+                                        fieldWithPath("upc").description("UPC of Beer"),
+                                        fieldWithPath("price").description("Price of Beer"),
+                                        fieldWithPath("quantityOnHand").description("Quantity on hand")
+                                )
+                        )
+                );
     }
 
     @Test
     void saveNewBeer() throws Exception {
         var beerDto = getValidBeerDto();
         var beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        ConstrainedFields fields = new ConstrainedFields(BeerDto.class);
+
         mockMvc.perform(post("/api/v1/beer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson)
-        ).andExpect(status().isCreated());
+        ).andExpect(status().isCreated())
+        .andDo(
+                document("v1/beer-post",
+                        requestFields(
+                                fields.withPath("id").ignored(),
+                                fields.withPath("version").ignored(),
+                                fields.withPath("createdDate").ignored(),
+                                fields.withPath("lastModifiedDate").ignored(),
+                                fields.withPath("beerName").description("Name of Beer"),
+                                fields.withPath("beerStyle").description("Style of Beer"),
+                                fields.withPath("upc").description("Beer UPC"),
+                                fields.withPath("price").description("Price of Beer"),
+                                fields.withPath("quantityOnHand").ignored()
+                        )
+                ));
     }
 
     @Test
@@ -59,5 +105,20 @@ class BeerControllerTest {
                 .price(new BigDecimal("2.99"))
                 .upc(12121231223L)
                 .build();
+    }
+
+    private static class ConstrainedFields {
+        private final ConstraintDescriptions constraintDescriptions;
+
+        public ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path)
+                    .attributes(
+                            key("constraints").value(StringUtils.collectionToDelimitedString(
+                                    this.constraintDescriptions.descriptionsForProperty(path), ". "
+                            )));
+        }
     }
 }
